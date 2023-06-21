49e258a35f764524fcf76bedbedd149cd0c0ea1c
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2023-06-21 20:48:57 +0900

    signature & verification test with SHA512/224

--- signature.py
+++ signature.py
@@ -2,6 +2,6 @@ from Crypto.PublicKey import RSA
 from Crypto.Hash import SHA512
 from Crypto.Signature import pss
 r=RSA.generate(2048)
-h=SHA512.new(b'Hello')
+h=SHA512.new(b'Hello',truncate='224')
 s=pss.new(r).sign(h)
 pss.new(r.publickey()).verify(h,s) # ValueError: Invalid signature
